8f35206a08f60e2346e96c744f2d0c19bc53ce2b
BahaKhmeissi <Bahaedd@gmail.com>
2022-04-02 17:27:30 +0100

    initialise project

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+export default function Header() {
+  return (
+    <header className="header">
+      <img src={require("../images/logo.png")} className="header--image" />
+      <h2 className="header--title">Meme Generator</h2>
+    
+    </header>
+  );
+}
